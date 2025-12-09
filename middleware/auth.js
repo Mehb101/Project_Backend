f1d9335aa2781dc5ce755f4ec75214afdac46fa5
Mehb101 <gulammehboobq@gmail.com>
2025-12-09 06:14:04 -0800

    first commit

---
+++ middleware/auth.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
+const expiration = "24h";
+
+function authMiddleware(req, res, next) {
+	let token = req.body?.token || req.query?.token || req.headers?.authorization;
+	if (req.headers.authorization) {
+		token = token.split(" ").pop().trim();
+	}
+	if (!token) {
+		next();
+	}
+	try {
+		const { data } = jwt.verify(token, secret, { maxAge: expiration });
+		req.user = data;
+	} catch {
+		console.log("Invalid token");
+	}
+	next();
+}
+
+function adminOnly(req, res, next) {
+	if (req.user && req.user.role === "admin") {
+		next(); 
+	} else {
+		res.status(403).json({ message: "Access denied. Admins only." });
+	}
+}
+
+function signToken({ username, email, _id }) {
+	const payload = { username, email, _id };
+	return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+}
+
+module.exports = {
+	authMiddleware,
+	adminOnly,
+	signToken,
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+
+require("dotenv").config();
+
+
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => console.log("Successfully connected to MongoDB!"))
+	.catch((err) => console.error("Connection error", err));
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+
+app.use(morgan("dev")); 
+app.use(express.json()); 
+
+require("./config/passport");
+
+
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/projects", require("./routes/projectRoutes"));
+app.use("/api", require("./routes/taskRoutes"));
+
+app.get("/", (req, res) => {
+	res.send("Welcome to my API!");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on port: ${PORT}`);
+});
